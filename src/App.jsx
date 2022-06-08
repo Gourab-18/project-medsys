bee40398b08ec59b35a5d67eabf50a78819c5968
Swagat Raj <shanu.r9971@gmail.com>
2022-06-08 10:15:56 +0530

    sss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import loginandresgister from "./components/loginandresgister/loginandresgister";
+
+function App() {
+  return (
+    <>
+      <loginandresgister/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/loginandresgister/loginandresgister.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loginandresgister = () => {
+  return (
+    <div>loginandresgister</div>
+  )
+}
+
+export default loginandresgister
