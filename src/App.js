28b68b24c8ff1f9fec65d3fdb95c948d301ba8e4
Swagat Raj <shanu.r9971@gmail.com>
2022-06-09 12:06:52 +0530

    added profile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginRegister from "./components/loginResgister/LoginRegister";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
-    <LoginRegister></LoginRegister>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginRegister />}/>
+        <Route path="/profile" element={<Profile />}/>
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
